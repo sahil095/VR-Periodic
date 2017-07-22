75b158cfda60f9e5e76e0e9c2e1d9999a9ace2b5
sahil095 <sehgal.sahil0786@gmail.com>
2017-07-22 17:51:07 +0530

    hover script and sprites added

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseOver : MonoBehaviour {
+
+
+	public GameObject myImage;
+	void OnMouseEnter() {
+		Debug.Log ("Mouse Enter");
+		myImage.SetActive(true);
+	}
+
+	void OnMouseExit() {
+		Debug.Log ("Mouse Exit");
+		myImage.SetActive (false);
+	}
+}
